866a36220abfa5b817ccb51e61fbdbe21bc09c48
Fajrun Shubhi <fajrunss7@gmail.com>
2023-03-16 23:57:48 +0700

    order detail fix

--- src/pages/OrderDetail/index.js
+++ src/pages/OrderDetail/index.js
@@ -1,54 +1,60 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {Header, ItemListFood, ItemValue, Button} from '../../components';
+import {Header, ItemListFood, ItemValue, Button, Gap} from '../../components';
 import {FoodDummy1} from '../../assets';
 
-const OrderDetail = ([navigation]) => {
+const OrderDetail = ({navigation}) => {
   return (
-    <View>
-      <Header
-        title="Payment"
-        subTitle="You deserve better meal"
-        onBack={() => {}}
-      />
-      <View style={styles.content}>
-        <Text style={styles.label}>Item Ordered</Text>
-        <ItemListFood
-          type="order-summary"
-          name="Sop Bumil"
-          image={FoodDummy1}
-          price="289.000"
-          items={5}
+    <ScrollView>
+      <View>
+        <Header
+          title="Payment"
+          subTitle="You deserve better meal"
+          onBack={() => {}}
         />
-        <Text style={styles.label}>Details Transaction</Text>
-        <ItemValue label="Cherry Healthy" value="IDR 18.390.000" />
-        <ItemValue label="Driver" value="IDR 50.000" />
-        <ItemValue label="Tax 10%" value="IDR 1.800.390" />
-        <ItemValue
-          label="Total Price"
-          value="IDR 390.803.000"
-          valueColor="#1ABC9C"
-        />
+
+        <View style={styles.content}>
+          <Text style={styles.label}>Item Ordered</Text>
+          <ItemListFood
+            type="order-summary"
+            name="Sop Bumil"
+            image={FoodDummy1}
+            price="289.000"
+            items={5}
+          />
+          <Text style={styles.label}>Details Transaction</Text>
+          <ItemValue label="Cherry Healthy" value="IDR 18.390.000" />
+          <ItemValue label="Driver" value="IDR 50.000" />
+          <ItemValue label="Tax 10%" value="IDR 1.800.390" />
+          <ItemValue
+            label="Total Price"
+            value="IDR 390.803.000"
+            valueColor="#1ABC9C"
+          />
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.label}>Deliver To: </Text>
+          <ItemValue label="Name" value="Fajrun Shubhi" />
+          <ItemValue label="Phone No." value="081223969871" />
+          <ItemValue label="Address" value="Cirebon, Jawa Barat" />
+          <ItemValue label="House No." value="No 1" />
+          <ItemValue label="City" value="Cirebon" />
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.label}>Order Status: </Text>
+          <ItemValue label="#FM209391" value="Paid" valueColor="#1ABC9C" />
+        </View>
+        <View style={styles.button}>
+          <Button
+            buttonName="Cancel My Order"
+            color="#D9435E"
+            textColor="white"
+            onPress={() => navigation.replace('SuccessOrder')}
+          />
+        </View>
+        <Gap height={40} />
       </View>
-      <View style={styles.content}>
-        <Text style={styles.label}>Deliver To: </Text>
-        <ItemValue label="Name" value="Fajrun Shubhi" />
-        <ItemValue label="Phone No." value="081223969871" />
-        <ItemValue label="Address" value="Cirebon, Jawa Barat" />
-        <ItemValue label="House No." value="No 1" />
-        <ItemValue label="City" value="Cirebon" />
-      </View>
-      <View style={styles.content}>
-        <Text style={styles.label}>Order Status: </Text>
-        <ItemValue label="#FM209391" value="Paid" valueColor="#1ABC9C" />
-      </View>
-      <View style={styles.button}>
-        <Button
-          buttonName="Checkot Now"
-          onPress={() => navigation.replace('SuccessOrder')}
-        />
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
